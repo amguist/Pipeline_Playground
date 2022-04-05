7aabf1d6a881199d1791837a368c44fbe1409cc7
Aaron Guist <rhon.guist@gmail.com>
2022-04-05 12:19:53 -0700

    Adding initial concord.yml file

--- scripts/transformer.py
+++ scripts/transformer.py
@@ -1,15 +1,15 @@
 def construct_inventory_file(inventoryFile, targets):
     targetList = []
-    with open(inventoryFile, "w") as file:
-        file.write("[MiddlewareServers]\n")
+    with open(inventoryFile, "w") as f:
+        f.write("[MiddlewareServers]\n")
         for target in targets:
             serverInventory = "%s\n" % target
-            file.write(serverInventory)
-            targetServers.append(serverInventory)
+            f.write(serverInventory)
+            targetList.append(serverInventory)
         execution.setVariable('targetList', targetList)
-        file.write("\n")
-        file.write("[MiddlewareServers:vars]\n")
-        file.write("ansible_connecton=ssh\n")
+        f.write("\n")
+        f.write("[MiddlewareServers:vars]\n")
+        f.write("ansible_connecton=ssh\n")
 
 def print_from_inventory_file(inventoryFile):
     with open(inventoryFile, "r") as file:
